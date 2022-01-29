a9308dbb9cc8d42bd152f0788dd593d1d6eb9200
Prathambhoir55 <80087152+Prathambhoir55@users.noreply.github.com>
2022-01-29 18:42:50 +0530

    calendar models

--- backend/Calendar/models.py
+++ backend/Calendar/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
+from accounts.models import User
 
-# Create your models here.
+class Contacts(models.Model):
+
+    name = models.CharField(max_length=100)
+    birthdate = models.DateField(format="%d-%m-%Y", input_formats=['%d-%m-%Y', 'iso-8601'])
+    phoneno = PhoneNumberField()
+
+
+class Events(models.Model):
+
+    name = models.CharField(max_length=100)
+    date = models.DateField(format="%d-%m-%Y", input_formats=['%d-%m-%Y', 'iso-8601'])
+    contacts = models.ManyToManyField("Contacts", related_name="events")
+    message = models.CharField(max_length=500)
+
