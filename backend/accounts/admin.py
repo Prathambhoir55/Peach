20c507ce8901a09d6ab0f21382ad9f2c9af3da6b
Prathambhoir55 <80087152+Prathambhoir55@users.noreply.github.com>
2022-01-29 09:32:42 +0530

    login signup

---
+++ backend/accounts/admin.py
@@ -0,0 +1,28 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from accounts.models import User
+
+# Register your models here.
+class UserAdmin(BaseUserAdmin):
+    model = User
+    list_display = ['email', 'name','is_staff','is_active','is_superuser']
+    list_filter = ['email','name','is_staff','is_active','is_superuser']
+
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal info', {'fields': ('name',),}),
+        ('Permissions', {'fields': ('is_active','is_staff','is_superuser')}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide,'),
+            'fields': ('email', 'password1', 'password2', 'name','is_staff','is_active'),
+        }),
+    )
+    search_fields = ('email',)
+    ordering = ('email',)
+    filter_horizontal = ()
+
+
+admin.site.register(User, UserAdmin)
